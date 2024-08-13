b8f90d5f0557f20f608bebf289fa0c886858d8d0
mrmorgan-i <misraelkelly@gmail.com>
2024-08-13 17:56:35 +0000

    link corrections

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,6 +1,6 @@
 import { getBlogPosts } from 'app/blog/utils'
 
-export const baseUrl = 'http://localhost:3000'
+export const baseUrl = 'https://mrimorgan.com'
 
 export default async function sitemap() {
   let blogs = getBlogPosts().map((post) => ({
